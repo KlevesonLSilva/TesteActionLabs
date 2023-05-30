3ac892a7fa23aaf51bf8d9e279499028ab5b3e3b
KlevesonLSilva <kleveson_x3@hotmail.com>
2023-05-30 14:30:07 -0300

    criando html e css simples e consumindo requisição

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,8 +9,11 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
   currencyForm: FormGroup;
-  exchangeRate: number | undefined;
+  exchangeRate: number = 0;
   expanded: boolean = false;
+  lastUpdatedAt: string = '';
+  dailyExchangeRates: any[] = [];
+  previousClose: number = 0;
 
 
  
@@ -29,16 +32,19 @@ export class HomeComponent implements OnInit {
   }
 
   getExchangeRate() {
-    this.getdailyExchangeRate();
     const formSymbolControl = this.currencyForm.get('fromSymbol');
     const fromSymbol = formSymbolControl ? formSymbolControl.value : '';
     const toSymbol = 'BRL';
   
     this.service.getCurrentExchangeRate(fromSymbol, toSymbol)
-      .subscribe((data: any) => {
-        this.exchangeRate = data?.exchange_rate;
-        // Processar a resposta da requisição aqui
-      });
+    .subscribe((data: any) => {
+      if (data.success) {
+        this.getdailyExchangeRate();
+        this.exchangeRate = data.exchangeRate.toFixed(2).replace('.', ',');
+        const lastUpdatedAt = new Date(data.lastUpdatedAt);
+        this.lastUpdatedAt = this.formatDate(lastUpdatedAt.toString());
+      }
+    });
   }
 
   getdailyExchangeRate() {
@@ -48,13 +54,49 @@ export class HomeComponent implements OnInit {
   
     this.service.getdailyExchangeRate(fromSymbol, toSymbol)
       .subscribe((data: any) => {
-        this.exchangeRate = data?.exchange_rate;
-        // Processar a resposta da requisição aqui
+        this.dailyExchangeRates = data.data;
+        this.calculateCloseDiff();
       });
   }
 
   toggleExpand() {
     this.expanded = !this.expanded;
+  }
+
+  formatDate(dateString: string) {
+    const date = new Date(dateString);
+    const day = ('0' + date.getDate()).slice(-2);
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const year = date.getFullYear().toString().slice(-2);
+  
+    return `${day}/${month}/${year}`;
+  }
+  
+  
+  formatNumberWithLeadingZero(number: number) {
+    return number < 10 ? `0${number}` : `${number}`;
+  }
+  
+  calculateCloseDiff() {
+    for (let i = 0; i < this.dailyExchangeRates.length; i++) {
+      const currentRate = this.dailyExchangeRates[i];
+      const previousRate = this.dailyExchangeRates[i - 1];
+      if (previousRate) {
+        currentRate.closeDiff = ((currentRate.close - previousRate.close) / previousRate.close) * 100;
+      } else {
+        currentRate.closeDiff = 0;
+      }
+    }
+  }
+
+  getDiffClass(diff: number) {
+    if (diff === null) {
+      return 'neutral';
+    } else if (diff > 0) {
+      return 'positive';
+    } else {
+      return 'negative';
+    }
   }
   
 
